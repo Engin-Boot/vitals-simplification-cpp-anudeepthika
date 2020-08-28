31eab3bc098a90ddb29d0fca933a74d126464c65
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-08-28 11:23:20 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,11 +1,31 @@
+#include <iostream>
 #include <assert.h>
 using namespace std;
- bool IsVitalInsideRange(float value, float lowerlimit, float upperlimit){
+ 
+class AlertWithSMS
+{
+  public:
+     void raiseAlert(const char* vitalNmae, const char* level)
+     {
+       cout << "SMS:" << vitalName << " " << level;
+     }
+};
+
+class AlertWithSound
+{
+  public:
+     void raiseAlert(const char* vitalNmae, const char* level)
+     {
+       cout << "SMS:" << vitalName << " " << level;
+     }
+};
+
+
+bool IsVitalInsideRange(float value, float lowerlimit, float upperlimit){
   return (value>=lowerlimit && value<=upperlimit);
  }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-//int vitalsresult;
 return (IsVitalInsideRange(bpm,70,150) && IsVitalInsideRange(spo2,90,100) && IsVitalInsideRange(respRate,30,95));
 }
 
