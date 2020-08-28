76c12c82bdc32612e4fbe8004511f94f83708f4d
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-08-28 12:10:37 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -26,13 +26,11 @@ bool IsVitalInsideRange(float value, float lowerlimit, float upperlimit){
  }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-return (IsVitalInsideRange(bpm,70,150) && IsVitalInsideRange(spo2,90,100) && IsVitalInsideRange(respRate,30,95));
+return (IsVitalInsideRange(bpm,70,150) && IsVitalInsideRange(spo2,90,101) && IsVitalInsideRange(respRate,30,95));
 }
 
 int main() {
-  assert(IsVitalInsideRange(70,60,100) == true);
-  assert(IsVitalInsideRange(40,60,100) == false);
-  assert(IsVitalInsideRange(120,60,100) == false);
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
+ bool result
 }
