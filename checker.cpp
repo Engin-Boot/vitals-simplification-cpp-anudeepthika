e1518d15c712ee59a2e9d2dc394daf526e3d708d
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-08-31 16:02:46 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 #include <assert.h>
 using namespace std;
- 
-class AlertWithSMS
+const int vitalLimitsArray[][2] = {{70,150},{90,101},{30,95}};
+class AlertSystem
 {
   public:
-     void raiseAlert(const char* vitalName, const char* level)
+    virtual void raiseAlert(const char* vitalName, const char* LevelIndicationMessage) = 0;
+};
+
+class AlertWithSMS : public AlertSystem
+{
+  public:
+     void raiseAlert(const char* vitalName, const char* LevelIndicationMessage)
      {
-       cout << "SMS:" << vitalName << " " << level;
+       cout << "SMS Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
      }
 };
 
-class AlertWithSound
+class AlertWithBuzzer : public AlertSystem
 {
   public:
-     void raiseAlert(const char* vitalName, const char* level)
+     void raiseAlert(const char* vitalName, const char* LevelIndicationMessage)
      {
-       cout << "SMS:" << vitalName << " " << level;
+       cout << "Buzzer Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
      }
 };
 
 
-bool IsVitalInsideRange(float value, float lowerlimit, float upperlimit){
+/*bool IsVitalInsideRange(float value, float lowerlimit, float upperlimit){
   return (value>=lowerlimit && value<=upperlimit);
  }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
 return (IsVitalInsideRange(bpm,70,150) && IsVitalInsideRange(spo2,90,101) && IsVitalInsideRange(respRate,30,95));
-}
+}*/
 
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+ /* assert(vitalsAreOk(80, 95, 60) == true);
+  assert(vitalsAreOk(60, 90, 40) == false);*/
+ cout<<"passed"<<endl;
 }
