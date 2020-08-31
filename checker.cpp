3ff022c7b974509909f96414cf679ada4fac2bb5
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-08-31 19:02:52 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,90 +1,43 @@
-#include <assert.h>
 #include <iostream>
-
-class AlertInterface
+#include <assert.h>
+using namespace std;
+const int vitalLimitsArray[][2] = {{70,150},{90,101},{30,95}};
+class AlertSystem
 {
   public:
-    virtual void raiseAlert(const char* vitalName, const char* level) = 0;
+    virtual void raiseAlert(const char* vitalName, const char* LevelIndicationMessage) = 0;
 };
 
-class AlertWithSMS: public AlertInterface
+class AlertWithSMS : public AlertSystem
 {
   public:
-    void raiseAlert(const char* vitalName, const char* level) override
-    {
-      std::cout << "SMS: " << vitalName << " " << level << std::endl;
-    }
+     void raiseAlert(const char* vitalName, const char* LevelIndicationMessage)
+     {
+       cout << "SMS Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
+     }
 };
 
-class AlertWithSound: public AlertInterface
+class AlertWithBuzzer : public AlertSystem
 {
   public:
-    void raiseAlert(const char* vitalName, const char* level) override
-    {
-      std::cout << "Sound: " << vitalName << " " << level << std::endl;  //'what'
-    }
+     void raiseAlert(const char* vitalName, const char* LevelIndicationMessage)
+     {
+       cout << "Buzzer Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
+     }
 };
 
-class AlertIntegrator : public AlertInterface
-{
-  private:
-    AlertWithSMS smsAlerter;
-    AlertWithSound soundAlerter;
-  public:
-    void raiseAlert(const char* vitalName, const char* level) override
-    {
-      smsAlerter.raiseAlert(vitalName, level);
-      soundAlerter.raiseAlert(vitalName, level);
-    }
-};
 
-class RangeChecker
-{
-  private:
-    int lower;
-    int upper;
-    const char* vitalName;
-    AlertInterface* alerter;
-  public:
-    RangeChecker(const char* name, int low, int up, AlertInterface* alerterPtr)
-    {
-      vitalName = name;
-      lower = low;
-      upper = up;
-      alerter = alerterPtr;
-    }
-    void checkAgainstRange(float value)
-    {
-      if(value < lower) {
-        alerter->raiseAlert(vitalName, "too low");  //'when'
-      } else if(value > upper) {
-        alerter->raiseAlert(vitalName, "too high");
-      }
-    }
-};
+/*bool IsVitalInsideRange(float value, float lowerlimit, float upperlimit){
+  return (value>=lowerlimit && value<=upperlimit);
+ }
 
-class VitalsIntegrator
-{
-  private:
-    RangeChecker bpmChecker, spo2Checker, respChecker;
-  public:
-    VitalsIntegrator(AlertInterface* alertPtr): 
-      bpmChecker("pulse rate", 70, 150, alertPtr),
-      spo2Checker("spo2", 90, 101, alertPtr),
-      respChecker("resp rate", 30, 95, alertPtr)
-    {}
-    void checkAllVitals(float bpm, float spo2, float respRate)
-    {
-      bpmChecker.checkAgainstRange(bpm);
-      spo2Checker.checkAgainstRange(spo2);
-      respChecker.checkAgainstRange(respRate);
-    }
-};
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+return (IsVitalInsideRange(bpm,70,150) && IsVitalInsideRange(spo2,90,101) && IsVitalInsideRange(respRate,30,95));
+}*/
 
 int main() {
-  AlertIntegrator alerter;  
-  VitalsIntegrator vitals(&alerter);
-
-  vitals.checkAllVitals(80, 95, 60);
-  vitals.checkAllVitals(60, 90, 40);
+ /* assert(vitalsAreOk(80, 95, 60) == true);
+  assert(vitalsAreOk(60, 90, 40) == false);*/
+  
+ cout<<"passed"<<endl;
 }
