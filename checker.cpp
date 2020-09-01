9c973e86f88cdf1bda2005425a71d51ebd9628b9
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-01 21:55:37 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,43 +1,78 @@
 #include <iostream>
 #include <assert.h>
+#include<string.h>
 using namespace std;
-const int vitalLimitsArray[][2] = {{70,150},{90,101},{30,95}};
 class AlertSystem
 {
-  public:
-    virtual void raiseAlert(const char* vitalName, const char* LevelIndicationMessage) = 0;
+public:
+    virtual void raiseAlert(string vitalName, const char* LevelIndicationMessage) = 0;
 };
 
 class AlertWithSMS : public AlertSystem
 {
-  public:
-     void raiseAlert(const char* vitalName, const char* LevelIndicationMessage)
-     {
-       cout << "SMS Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
-     }
+public:
+    void raiseAlert(string vitalName, const char* LevelIndicationMessage)
+    {
+        cout << "SMS Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
+    }
 };
 
 class AlertWithBuzzer : public AlertSystem
 {
-  public:
-     void raiseAlert(const char* vitalName, const char* LevelIndicationMessage)
-     {
-       cout << "Buzzer Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
-     }
+public:
+    void raiseAlert(string vitalName, const char* LevelIndicationMessage)
+    {
+        cout << "Buzzer Alert:" << vitalName << " --- " << LevelIndicationMessage << endl;
+    }
 };
 
+class PatientVitals
+{
+private:
+    string* vitalName;
+    float* vitalValue;
+    float* vitalLowerLimit;
+    float* vitalUpperLimit;
+public:
+    PatientVitals(string* name, float* value, float* LowerLimit, float* UpperLimit) : vitalName(name), vitalValue(value), vitalLowerLimit(LowerLimit), vitalUpperLimit(UpperLimit) { }
+    
+    void checkVital(int totalNoOfVitalsToBeChecked, string* name, float* value, float* Lower, float* Upper, AlertSystem* alertptr)
+    {
+        for (int i = 0; i < totalNoOfVitalsToBeChecked; i++)
+        {
+            if (value[i] < Lower[i])
+            {
+                alertptr->raiseAlert(name[i], "TOO LOW!!!");
+            }
+            else if (value[i] >= Upper[i])
+            {
+                alertptr->raiseAlert(name[i], "TOO HIGH!!!");
+            }
+        }
+        
+    }
+};
 
-/*bool IsVitalInsideRange(float value, float lowerlimit, float upperlimit){
-  return (value>=lowerlimit && value<=upperlimit);
- }
+int main()
+{
+    /*Depending on the age group of the patient limits of vital range may differ!! So provide the lower and upper limits in the order of the vitals in the arrays respectively*/
+    /*For eg.: respiratory rate is different for different age groups*/
+    /*every patient may not require all the vital check. Here we can provide the required vitals for checking*/
+    AlertWithSMS alertwithsms;
+    AlertWithBuzzer alertwithbuzzer;
+    cout << "....Vital check for patient1...." << endl;
+    string vitalNames1[] = {"bpm","spo2","respRate"}; //patient1 needs check for 3 vitals
+    float vitalLowerLimits1[] = { 70,90,24 };
+    float vitalUpperLimits1[] = { 150,100,30 };
+    float vitalValues1[] = { 60,93,35 };
+    PatientVitals Patient1(vitalNames1, vitalValues1, vitalLowerLimits1, vitalUpperLimits1);
+    Patient1.checkVital(3,vitalNames1, vitalValues1, vitalLowerLimits1, vitalUpperLimits1, &alertwithsms); //alerting with SMS
+    cout << "....Vital check for patient2...." << endl;
+    string vitalNames2[] = { "bpm","spo2","respRate","Body Temperature"};
+    float vitalLowerLimits2[] = { 70,90,12,97};  //patient2 needs check for 3 vitals
+    float vitalUpperLimits2[] = { 150,100,20,99 };
+    float vitalValues2[] = { 90,83,10,100};
+    PatientVitals Patient2(vitalNames2, vitalValues2, vitalLowerLimits2, vitalUpperLimits2);
+    Patient2.checkVital(4, vitalNames2, vitalValues2, vitalLowerLimits2, vitalUpperLimits2, &alertwithbuzzer); //alerting with buzzer sound
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-return (IsVitalInsideRange(bpm,70,150) && IsVitalInsideRange(spo2,90,101) && IsVitalInsideRange(respRate,30,95));
-}*/
-
-int main() {
- /* assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);*/
- /*aim: Write for a different patient with different vitals and different limits e.g: p1= {{"bpm",60,70,150,&alertwithsms},{"spo2",93,90,100,&alertwithbuzzer},...}*/
- cout<<"passed"<<endl;
 }
